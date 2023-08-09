0d0aeb2e1ec53da1e8019d392364a6c854b1ac36
Ramazan <airtribunal@gmail.com>
2023-08-09 14:47:27 +0300

    homepage

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -2,18 +2,60 @@ import { Box, Link, Typography } from "@mui/material";
 import React from "react";
 import { useNavigate } from "react-router";
 
-
 const Navbar = () => {
-    const navigate = useNavigate()
+    const navigate = useNavigate();
     return (
-        <Box component="div" className="container">
-            <Box component="div" sx={{display: 'flex', justifyContent: 'space-between'}}>
-                <Typography component="h1">logo</Typography>
-                <Box component="div">
-                    <Link onClick={() => navigate('/gggg')}>главная</Link>
-                    <Link onClick={() => navigate('/gggg')}>фильмы</Link>
-                    <Link onClick={() => navigate('/gggg')}>избранное</Link>
-                    <Link onClick={() => navigate('/gggg')}>контакты</Link>
+        <Box
+            sx={{
+                position: "absolute",
+                left: "0",
+                right: "0",
+                paddingTop: "1rem",
+            }}
+            className="container"
+        >
+            <Box
+                component="div"
+                sx={{ display: "flex", justifyContent: "space-between" }}
+            >
+                <Typography
+                    sx={{ fontSize: "25px", fontWeight: "600", color: "#fff" }}
+                    component="h1"
+                >
+                    Just Movies
+                </Typography>
+                <Box
+                    sx={{
+                        width: "100%",
+                        maxWidth: "400px",
+                        display: "flex",
+                        justifyContent: "space-between",
+                    }}
+                >
+                    <Link
+                        sx={{ color: "#fff", cursor: 'pointer' }}
+                        onClick={() => navigate("/gggg")}
+                    >
+                        главная
+                    </Link>
+                    <Link
+                        sx={{ color: "#fff", cursor: 'pointer' }}
+                        onClick={() => navigate("/gggg")}
+                    >
+                        фильмы
+                    </Link>
+                    <Link
+                        sx={{ color: "#fff", cursor: 'pointer' }}
+                        onClick={() => navigate("/gggg")}
+                    >
+                        избранное
+                    </Link>
+                    <Link
+                        sx={{ color: "#fff", cursor: 'pointer'}}
+                        onClick={() => navigate("/gggg")}
+                    >
+                        контакты
+                    </Link>
                 </Box>
             </Box>
         </Box>
--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,12 +1,93 @@
 import { Button, Typography, Box } from "@mui/material";
 import React from "react";
 import { Link } from "react-router-dom";
-
+import ironManImg from "../images/her-img.png";
 function Home() {
     return (
-        <>
-            
-        </>
+        <Box
+            sx={{
+                display: "flex",
+                alignItems: "center",
+                justifyContent: "center",
+                width: "100%",
+                height: "100vh",
+                background:
+                    "linear-gradient(45deg, rgba(0, 0, 0, 0.86) 1.30%, #8F1414 59.38%)",
+            }}
+        >
+            <Box className="container">
+                <Box
+                    sx={{
+                        display: "flex",
+                        justifyContent: "space-between",
+                        alignItems: 'center',
+                        padding: "40px",
+                        width: "100%",
+                        maxWidth: "1170px",
+                        height: "70vh",
+                        background: "rgba(255, 255, 255, 0.10)",
+                        boxShadow:
+                            "4px 4px 13px 0px rgba(255, 255, 255, 0.50);",
+                        borderRadius: "10px",
+                    }}
+                >
+                    <Box>
+                        <Typography
+                            sx={{
+                                fontSize: "45px",
+                                fontWeight: "800",
+                                maxWidth: "470px",
+                            }}
+                            component="h1"
+                        >
+                            Смотреть фильмы с{" "}
+                            <Typography
+                                component="h1"
+                                sx={{
+                                    color: "#CD0B0B",
+                                    fontSize: "inherit",
+                                    fontWeight: "inherit",
+                                }}
+                            >
+                                Just Movies стало
+                            </Typography>{" "}
+                            <Typography
+                                component="h1"
+                                sx={{
+                                    color: "#CD0B0B",
+                                    fontSize: "inherit",
+                                    fontWeight: "inherit",
+                                }}
+                            >
+                                проще.
+                            </Typography>{" "}
+                        </Typography>
+                        <Typography component="p" sx={{ maxWidth: "300px" }}>
+                            Мы предлагаем лучший сервис по просмотру фильмов в
+                            любой точке мира и в любое время
+                        </Typography>
+                        <Button
+                            sx={{
+                                width: "100%",
+                                maxWidth: "400px",
+                                marginTop: "1.25rem",
+                                fontWeight: "600",
+                                fontSize: "18px",
+                                textTransform: "initial",
+                                background: "#A71A1A",
+                                boxShadow: "0px 4px 8px 0px #A71A1A",
+                            }}
+                            variant="contained"
+                        >
+                            Смотреть сейчас
+                        </Button>
+                    </Box>
+                    <Box>
+                        <Box component="img" src={ironManImg} />
+                    </Box>
+                </Box>
+            </Box>
+        </Box>
     );
 }
 

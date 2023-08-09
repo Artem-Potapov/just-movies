582440fc7102623abc1ee6470b604c68228abd18
Ramazan <airtribunal@gmail.com>
2023-08-09 15:45:52 +0300

    template for axios

--- src/App.jsx
+++ src/App.jsx
@@ -1,32 +1,39 @@
-import { createBrowserRouter, Navigate, RouterProvider } from "react-router-dom";
+import {
+    createBrowserRouter,
+    Navigate,
+    RouterProvider,
+} from "react-router-dom";
 import AppLayout from "./components/AppLayout";
 import ErrorElement from "./components/Error";
 import Home from "./pages/Home";
+import { useEffect, useState } from "react";
+import axios from "axios";
+const url = "https://jsonplaceholder.typicode.com/posts/";
 
 function App() {
 
-  const router = createBrowserRouter(
-  [
-    {
-      path: "/",
-      element: <AppLayout />,
-      errorElement: <ErrorElement />,
-      children: [
-      {
-        index: true,
-        element: <Navigate to="/home" />,
-      },
-      {
-        path: "/home",
-        element: <Home />,
-      }]
-    }
-  ],)
+    useEffect(() => {
+      
+    }, [url]);
+    const router = createBrowserRouter([
+        {
+            path: "/",
+            element: <AppLayout />,
+            errorElement: <ErrorElement />,
+            children: [
+                {
+                    index: true,
+                    element: <Navigate to="/home" />,
+                },
+                {
+                    path: "/home",
+                    element: <Home />,
+                },
+            ],
+        },
+    ]);
 
-
-  return (
-    <RouterProvider router={router} />
-  );
+    return <RouterProvider router={router} />;
 }
 
 export default App;

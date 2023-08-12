71e53f1cfb896e366a0bce8776d9ef1bf6511c4b
Artem-Potapov <potapov23500@gmail.com>
2023-08-12 14:40:37 +0300

    add MovieCard

--- src/components/molecules/MovieCard.jsx
+++ src/components/molecules/MovieCard.jsx
@@ -1,17 +1,38 @@
-import { Box, Typography } from "@mui/material";
+import { Box, Button, Typography } from "@mui/material";
 import React from "react";
 
-
-function MovieCard(props)
-{
-    return(
-        <Box sx={{width: "1070px", height: "200px", background: "#FFF"}}>
-            <Typography sx={{color: "#0F0F0F",}} component={"h2"}>{props.title}</Typography>
-            <Typography sx={{color: "#0F0F0F",}} component={"p"}>{props.description}</Typography>
-            <Typography sx={{color: "#0F0F0F",}} component={"p"}>{props.time}</Typography>
-            <Typography sx={{color: "#0F0F0F",}} component={"p"}>Цена: {props.price}</Typography>
-        </Box>
-    )
+function MovieCard(props) {
+  return (
+    <Box
+      sx={{
+        width: "100%",
+        height: "300px",
+        background: "#F0F0F0",
+        margin: "0 20px",
+        padding: "5px 10px",
+        border: "groove 0.7em red;",
+        borderRadius: "1em",
+      }}
+    >
+      <Typography sx={{ color: "#FA0DD8" }} component={"p"}>
+        Тут была картинка :\
+      </Typography>
+      <Typography sx={{ color: "#0F0F0F", fontSize: "20px" }} component={"h2"}>
+        {props.title}
+      </Typography>
+      <Typography sx={{ color: "#0F0F0F" }} component={"p"}>
+        {props.description}
+      </Typography>
+      <Typography sx={{ color: "#0F0F0F" }} component={"p"}>
+        {props.time}
+      </Typography>
+      <Typography sx={{ color: "#0F0F0F" }} component={"p"}>
+        Цена: {props.price}
+      </Typography>
+      <Button>Смотреть сейчас</Button>
+      <Button>В избранное</Button>
+    </Box>
+  );
 }
 
 export default MovieCard;
--- src/pages/Movies.jsx
+++ src/pages/Movies.jsx
@@ -2,30 +2,37 @@ import React from "react";
 import { Box, Typography } from "@mui/material";
 import MovieCard from "../components/molecules/MovieCard";
 
-function Movies(props)
-{
+function Movies(props) {
+  const moviesArray = props.data.map((movie) => {
+    return (
+      <MovieCard
+        title={movie.attributes.movieTitle}
+        description={movie.attributes.movieDesc}
+        time={movie.attributes.movieTime}
+        price={movie.attributes.moviePrice}
+      />
+    );
+  });
 
-    const moviesArray = props.data.map((movie) => {
-        return(
-            <MovieCard title={movie.attributes.movieTitle} description={movie.attributes.movieDesc} time={movie.attributes.movieTime} price={movie.attributes.moviePrice} />
-        )
-    })
-
-    return(
-        <Box
-            sx={{
-                display: "flex",
-                alignItems: "center",
-                justifyContent: "center",
-                width: "100%",
-                height: "100vh",
-                background:
-                    "linear-gradient(45deg, rgba(0, 0, 0, 0.86) 1.30%, #8F1414 59.38%)",
-            }}
-        >    
-            {moviesArray}
+  return (
+    <Box
+      sx={{
+        display: "flex",
+        alignItems: "center",
+        justifyContent: "center",
+        width: "100%",
+        height: "100vh",
+        background:
+          "linear-gradient(45deg, rgba(0, 0, 0, 0.86) 1.30%, #8F1414 59.38%)",
+      }}
+    >
+      <Box className="container">
+        <Box sx={{ flexDirection: "column", marginTop: "5rem" }}>
+          {moviesArray}
         </Box>
-    )
+      </Box>
+    </Box>
+  );
 }
 
 export default Movies;

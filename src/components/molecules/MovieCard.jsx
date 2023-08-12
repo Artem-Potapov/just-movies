0f0fad2380dd3b865fc8659c913f311649b514e8
Artem-Potapov <potapov23500@gmail.com>
2023-08-12 15:15:41 +0300

    customize MovieCard

--- src/components/molecules/MovieCard.jsx
+++ src/components/molecules/MovieCard.jsx
@@ -8,29 +8,43 @@ function MovieCard(props) {
         width: "100%",
         height: "300px",
         background: "#F0F0F0",
-        margin: "0 20px",
+        margin: "20px",
         padding: "5px 10px",
         border: "groove 0.7em red;",
         borderRadius: "1em",
+        display: "flex",    
+        flexDirection: "row",
+        justifyContent: "space-between",
+        alignItems: "center",
       }}
     >
-      <Typography sx={{ color: "#FA0DD8" }} component={"p"}>
-        Тут была картинка :\
-      </Typography>
-      <Typography sx={{ color: "#0F0F0F", fontSize: "20px" }} component={"h2"}>
-        {props.title}
-      </Typography>
-      <Typography sx={{ color: "#0F0F0F" }} component={"p"}>
-        {props.description}
-      </Typography>
-      <Typography sx={{ color: "#0F0F0F" }} component={"p"}>
-        {props.time}
-      </Typography>
-      <Typography sx={{ color: "#0F0F0F" }} component={"p"}>
-        Цена: {props.price}
-      </Typography>
-      <Button>Смотреть сейчас</Button>
-      <Button>В избранное</Button>
+      <Box sx={{height: "100%", width: "200px"}}>
+        <Typography sx={{ color: "#FA0DD8" }} component={"p"}>
+          Тут была картинка :\
+        </Typography>
+      </Box>
+      <Box sx={{display: "flex", flexDirection: "column", width: "500px"}}>
+        <Typography
+          sx={{ color: "#0F0F0F", fontSize: "20px", marginBottom: "10px", }}
+          component={"h2"}
+        >
+          {props.title}
+        </Typography>
+
+        <Typography sx={{ color: "#0F0F0F" }} component={"p"}>
+          {props.description}
+        </Typography>
+        {/* <Typography sx={{ color: "#0F0F0F" }} component={"p"}>
+          {props.time}
+        </Typography>
+        <Typography sx={{ color: "#0F0F0F" }} component={"p"}>
+          Цена: {props.price}
+        </Typography> */}
+      </Box>
+      <Box sx={{display: "flex", flexDirection: "column", maxWidth: "50%", }}>
+        <Button variant="contained" sx={{marginBottom:"40px", background: "linear-gradient(90deg, #A71A1A 0%, #BB1616 100%);"}}>Смотреть сейчас</Button>
+        <Button variant="contained" sx={{marginBottom:"40px", background: "linear-gradient(90deg, #A71A1A 0%, #BB1616 100%);"}} >В избранное</Button>
+      </Box>
     </Box>
   );
 }
